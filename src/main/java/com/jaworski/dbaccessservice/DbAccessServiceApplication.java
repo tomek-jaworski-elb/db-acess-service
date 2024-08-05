69483409f773b9b690c4b3a5a115881ed87911d7
Tomasz Jaworski <tomasz.jaworski@adtran.com>
2024-08-05 07:59:26 +0200

    Initial commit

---
+++ src/main/java/com/jaworski/dbaccessservice/DbAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jaworski.dbaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAccessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbAccessServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaworski/dbaccessservice/DbAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaworski.dbaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbAccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
